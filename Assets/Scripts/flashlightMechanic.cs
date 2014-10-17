bb2c89dc3d64073a85ea6d2de2d7cfbb14b6d5b5
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2014-10-17 19:24:47 -0400

    Flashlight mechanic is done

--- Assets/Scripts/flashlightMechanic.cs
+++ Assets/Scripts/flashlightMechanic.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class flashlightMechanic : MonoBehaviour {
 
+	public GameObject flashLight;
+	public bool isLightOn;
+
 	// Use this for initialization
 	void Start () {
-	
+		isLightOn = true;
+		flashLight = transform.GetChild(1).GetChild(0).gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(isLightOn){
+			flashLight.SetActive(true);
+		} else {
+			flashLight.SetActive(false);
+		}
+
+		if(Input.GetKeyDown(KeyCode.RightShift)){
+			isLightOn = !isLightOn;
+		}
 	}
 }
