9a61481b4ae7c957bcfbd9b0a1f42d70b341a8a7
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2014-10-17 21:20:27 -0400

    Working on Slasher AI

---
+++ Assets/Scripts/slasherAI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slasherAI : MonoBehaviour {
+
+	public GameObject[] victims;
+	public Transform closestVictim;
+	public bool isPursuing;
+
+	// Use this for initialization
+	void Start () {
+		victims = GameObject.FindGameObjectsWithTag("Player");
+		closestVictim = victims[Random.Range(0,victims.Length)].transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isPursuing){
+			GetComponent<NavMeshAgent>().SetDestination(closestVictim.position);
+		}
+	}
+}
