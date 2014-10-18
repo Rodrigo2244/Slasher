2985c30039a41a5f34b3597fad3c73eb20afe316
Krondras <ajb3000@hotmail.com>
2014-10-17 20:49:26 -0400

    Checking in some major Stuff related to spawning

--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -4,9 +4,12 @@ using System.Collections.Generic;
 
 public class SpawnPlayers : MonoBehaviour {
 	public GameObject Player;
+	public int playerNum;
+	public GameObject controller;
 	// Use this for initialization
 	void Start () {
-		int playerNum = 4;
+		controller = GameObject.Find("Game Controller");
+		playerNum = controller.GetComponent <GameController>().playerNum;
 		var respawns = new List<GameObject>();
 
 		foreach (GameObject respawn in GameObject.FindGameObjectsWithTag ("Respawn")) {
@@ -14,6 +17,10 @@ public class SpawnPlayers : MonoBehaviour {
 		}
 
 		for (var i = 0; i < playerNum; i++) {
+			Player.GetComponent <MouseLook>().PlayerId = i;
+			Player.GetComponent <FPSInputController>().PlayerId = i;
+			Player.GetComponentInChildren <MouseLook>().PlayerId = i;
+
 			var index = Random.Range (0, respawns.Count - 1);
 			Instantiate(Player, respawns[index].transform.position, respawns[index].transform.rotation );
 			Debug.Log (respawns[index].name);
