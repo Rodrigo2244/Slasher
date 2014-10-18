c4812b1641c8e93b41b70aab18bdebb566a8ad9e
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2014-10-18 17:12:31 -0400

    Intro Matinee

---
+++ Assets/Scripts/introMatinee.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class introMatinee : MonoBehaviour {
+
+	public GameObject gui;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(animation.isPlaying == false){
+			gui.SetActive(true);
+		}
+	}
+}
