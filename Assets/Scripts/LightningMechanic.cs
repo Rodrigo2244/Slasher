eb367a1535de876f1957d24416231bb5e1022fe1
Eric Fraze <magnilucent@gmail.com>
2014-10-18 10:29:14 -0400

    Added lightning script & prefab

---
+++ Assets/Scripts/LightningMechanic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightningMechanic : MonoBehaviour {
+	private bool flash = false;
+	public float maxIntensity = 8;
+	public float lightSpeed = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Flash () {
+		flash = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range (0, 100) == 5)
+			flash = true;
+
+		if (flash == true) {
+			if (light.intensity < maxIntensity) {
+				light.intensity += lightSpeed;
+			}else{
+				flash = false;
+			}
+		} else {
+			if (light.intensity > 0) {
+				light.intensity -= lightSpeed;
+			} else {
+				light.intensity -= 0;
+			}
+		}
+	}
+}
