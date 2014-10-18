25a911bf1dafdf377f685228bdcf7564c17cf4c9
Krondras <ajb3000@hotmail.com>
2014-10-18 12:07:45 -0400

    Whole bunch of changes

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -3,17 +3,18 @@ using System.Collections;
 
 public class DoorController : MonoBehaviour {
 	public bool open = false;
-	
-
+	public bool delay = false;
+	public GameObject[] players;
 	// Use this for initialization
 	void Start () {
-
+		players = GameObject.FindGameObjectsWithTag("Player");
 	}
 
 	public IEnumerator Open () {
+		animation["Door Open"].speed = 1;
 		animation.Play("Door Open");
 		yield return new WaitForSeconds( animation["Door Open"].length );
-		open = true;
+		delay = false;
 		Debug.Log ("Open");
 	}
 
@@ -21,12 +22,43 @@ public class DoorController : MonoBehaviour {
 		animation ["Door Open"].speed = -1;
 		animation.Play("Door Open");
 		yield return new WaitForSeconds( animation["Door Open"].length );
-		open = false;
+		delay = false;
 		Debug.Log ("Closed");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		foreach(GameObject player in players)
+		{
+			if(Input.GetAxis("Flashlight"+player.GetComponent<FPSInputController>().PlayerId) == 1 && delay != true && Vector3.Distance(player.transform.position, gameObject.transform.position) <= 2)
+			{
+				delay = true;
+				if(open)
+				{
+					open = false;
+					StartCoroutine("Close");
+					Debug.Log ("Actually Did something");
+
+				}
+				else
+				{
+					open = true;
+					StartCoroutine("Open");
+
+				}
+			}
+		}
+		
+	}
+	void OnTriggerStay(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			if(Input.GetAxis("Flashlight"+other.GetComponent<FPSInputController>().PlayerId) == 1)
+			{
+
+				
+			}
+		}
 	}
 }
