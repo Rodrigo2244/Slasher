b91c1cc9163439aabb5e4230bebaf3f68a86abe3
Cobra034 <sam.heineman034@gmail.com>
2014-10-18 13:44:18 -0400

    game controller update for end game condition

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,7 +8,24 @@ public class GameController : MonoBehaviour {
 	public float musicVol;
 	public bool isPaused;
 
+	public bool[] hasWon;
+	public bool[] hasDied;
+
 	void Awake(){
 		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	void Update(){
+		hasWon = new bool[numPlayers];
+		hasDied = new bool[numPlayers];
+		for(int i = 0; i < numPlayers; i++){
+			if(hasWon[i] == true || hasDied[i] == true){
+				if(i == numPlayers - 1)
+					print ("YAY");
+					Application.LoadLevel("ScoreBoard");
+			}
+			else
+				break;
+		}
 	}
 }
