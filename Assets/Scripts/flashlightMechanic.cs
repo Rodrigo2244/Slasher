8cfb4e483a57bdcd99df0158ac799859fdbe5fbf
Eric Fraze <magnilucent@gmail.com>
2014-10-18 16:44:34 -0400

    Fixed flashlight conflict

--- Assets/Scripts/flashlightMechanic.cs
+++ Assets/Scripts/flashlightMechanic.cs
@@ -4,17 +4,10 @@ using System.Collections;
 public class flashlightMechanic : MonoBehaviour {
 	public int PlayerId = 1;
 	public Light flashLight;
-<<<<<<< HEAD
-<<<<<<< HEAD
 	public bool isLightOn;
 	public AudioSource FlashlightClick;
-=======
-	public bool isLightOn = true;
->>>>>>> 4ab5cd7313e7525c90d480ec44f88f45e604be25
-=======
-	public bool isLightOn = true;
->>>>>>> 8d1eba086336b6824613153a736f974f967f907e
 	bool Held = false;
+
 	// Use this for initialization
 	void Start () {
 		isLightOn = true;
