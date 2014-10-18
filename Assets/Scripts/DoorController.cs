db5950661deb7536b178bd4d5816a6c8bd291bb9
Eric Fraze <magnilucent@gmail.com>
2014-10-17 23:23:47 -0400

    Added basic door

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour {
+	public bool open = false;
+	
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public IEnumerator Open () {
+		animation.Play("Door Open");
+		yield return new WaitForSeconds( animation["Door Open"].length );
+		open = true;
+		Debug.Log ("Open");
+	}
+
+	public IEnumerator Close () {
+		animation ["Door Open"].speed = -1;
+		animation.Play("Door Open");
+		yield return new WaitForSeconds( animation["Door Open"].length );
+		open = false;
+		Debug.Log ("Closed");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
