b50cc4a1c6c4a8f3d9087951d5d4a0bc59712b24
Eric Fraze <magnilucent@gmail.com>
2014-10-18 15:10:16 -0400

    Added Flash function

--- Assets/Scripts/LightningMechanic.cs
+++ Assets/Scripts/LightningMechanic.cs
@@ -4,17 +4,21 @@ using System.Collections;
 public class LightningMechanic : MonoBehaviour {
 	private bool flash = false;
 	public float maxIntensity = 8;
-	public float lightSpeed = 1.5f;
+	public float lightSpeed = 0.5f;
 	public int flashChance = 0;
 	// Use this for initialization
 	void Start () {
 
 	}
-	
+
+	public void Flash() {
+		flash = true;
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (Random.Range (0, flashChance) == 5)
-			flash = true;
+			Flash();
 
 		if (flash == true) {
 			if (light.intensity < maxIntensity) {
