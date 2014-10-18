0fed1a32450debb50c514403b9d3195846580bdd
Eric Fraze <magnilucent@gmail.com>
2014-10-18 16:51:02 -0400

    Changed timing of flashlight switch

--- Assets/Scripts/flashlightMechanic.cs
+++ Assets/Scripts/flashlightMechanic.cs
@@ -15,7 +15,7 @@ public class flashlightMechanic : MonoBehaviour {
 	}
 
 	public IEnumerator Switch () {
-		yield return new WaitForSeconds (0.3f);
+		yield return new WaitForSeconds (0.2f);
 		isLightOn = !isLightOn;
 	}
 	
