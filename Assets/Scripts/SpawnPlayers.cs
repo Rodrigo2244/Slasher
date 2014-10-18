24c56bfb251af99e628e7c897bc4d282ddbc91c7
Eric Fraze <magnilucent@gmail.com>
2014-10-17 20:08:44 -0400

    Basic player spawn script

---
+++ Assets/Scripts/SpawnPlayers.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnPlayers : MonoBehaviour {
+	public GameObject Player;
+	// Use this for initialization
+	void Start () {
+		int playerNum = 4;
+		var respawns = new List<GameObject>();
+
+		foreach (GameObject respawn in GameObject.FindGameObjectsWithTag ("Respawn")) {
+			respawns.Add(respawn);
+		}
+
+		for (var i = 0; i < playerNum; i++) {
+			var index = Random.Range (0, respawns.Count - 1);
+			Instantiate(Player, respawns[index].transform.position, respawns[index].transform.rotation );
+			Debug.Log (respawns[index].name);
+			respawns.RemoveAt(index);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
