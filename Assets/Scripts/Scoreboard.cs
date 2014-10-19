288ba201f093ab21e14bea6eadd09ec083d709b6
Cobra034 <sam.heineman034@gmail.com>
2014-10-19 13:53:06 -0400

    updated scoreboard

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -27,9 +27,9 @@ public class Scoreboard : MonoBehaviour {
 		buttonRect= new Rect(Screen.width / 2-75, (2 * Screen.height / 3) ,150,60);
 		for(int i = 0; i < numPlayers; i++){
 			if(hasWon[i] == true)
-				GUI.Label(new Rect(Screen.width/2-80, Screen.height/2+i*20-300, 250, 200), "Player " +(i+1)+" has survived.",gui);
+				GUI.Label(new Rect(Screen.width/2-100, Screen.height/2+i*60-250, 250, 200), "Player " +(i+1)+" has survived.",gui);
 			if(hasDied[i] == true)
-				GUI.Label(new Rect(Screen.width/2-80, Screen.height/2+i*20-300, 250, 200), "Player " +(i+1)+" has died.",gui);
+				GUI.Label(new Rect(Screen.width/2-100, Screen.height/2+i*60-250, 250, 200), "Player " +(i+1)+" has died.",gui);
 		}
 		if(GUI.Button(buttonRect,"Return to Menu",gui)){
 			Destroy (gameController);
