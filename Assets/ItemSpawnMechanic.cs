c4c7c51ef31c7ac713315658a30079e124569766
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2014-10-19 08:48:05 -0400

    Changed the item spawning script

--- Assets/ItemSpawnMechanic.cs
+++ Assets/ItemSpawnMechanic.cs
@@ -4,26 +4,16 @@ using System.Collections.Generic;
 
 public class ItemSpawnMechanic : MonoBehaviour {
 
+	public GameObject[] spawnPoints;
+	public GameObject[] itemList;
+
 	// Use this for initialization
 	void Start () {
-		var itemList = new List<GameObject>();
 
-		foreach (GameObject item in GameObject.FindGameObjectsWithTag ("Item")) {
-			itemList.Add(item);
-		}
-		if(itemList.Count > 0)
-		{
-			foreach (GameObject ItemSpawn in GameObject.FindGameObjectsWithTag ("ItemSpawn")) {
+		spawnPoints = GameObject.FindGameObjectsWithTag("ItemSpawn");
 
-				GameObject item = itemList[ Random.Range (0, itemList.Count)  ];
-
-				GameObject newItem = Instantiate(item, ItemSpawn.transform.position, item.transform.rotation ) as GameObject;
-
-				newItem.transform.parent = ItemSpawn.transform;
-
-				newItem.transform.Rotate(newItem.transform.up * Random.Range(0, 361) );
-
-			}
+		foreach(GameObject point in spawnPoints){
+			Instantiate(itemList[Random.Range(0,itemList.Length)],point.transform.position,point.transform.rotation);
 		}
 	}
 }
