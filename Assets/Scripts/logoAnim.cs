6d0a30d83fb62a51b172adac0cea1634aaae7046
Rodrigo2244 <rodrigo_rojas_ferrer@hotmail.com>
2014-10-19 13:21:12 -0400

    Added team logo

---
+++ Assets/Scripts/logoAnim.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logoAnim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(startGame());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator startGame(){
+		yield return new WaitForSeconds(7);
+		Application.LoadLevel("Menus");
+	}
+}
