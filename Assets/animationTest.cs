7844649803a47e8b7efac523dc480e70c4761c2f
Krondras <ajb3000@hotmail.com>
2014-10-18 23:20:38 -0400

    Pushing animation changes

---
+++ Assets/animationTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animationTest : MonoBehaviour {
+
+	private Animation _animation;
+
+	// Use this for initialization
+	void Start () {
+		_animation = transform.GetChild(0).GetComponent<Animation>();
+		_animation.Play("Take 001");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
