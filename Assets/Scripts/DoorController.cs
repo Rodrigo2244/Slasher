3dd156055a69d1785722935566d98651904093a5
Krondras <ajb3000@hotmail.com>
2014-10-19 13:07:36 -0400

    Doors fully fixed

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -8,6 +8,7 @@ public class DoorController : MonoBehaviour {
 	public GameObject enemy;
 	public AudioSource doorClose;
 	public AudioSource doorOpen;
+	RaycastHit[] hit;
 
 	// Use this for initialization
 	void Start () {
@@ -40,12 +41,15 @@ public class DoorController : MonoBehaviour {
 
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
+
 		if( (enemy.transform.position - gameObject.transform.position).magnitude <= 2)
 		{
 			Debug.Log ("Walked through Slasher");
 			enemy.GetComponent<slasherAI>().wait = true;
 			enemy.GetComponent<slasherAI>().door = gameObject;
+			delay = true;
 			if(open != true)
 			{
 				StartCoroutine("Open");
@@ -58,20 +62,34 @@ public class DoorController : MonoBehaviour {
 		}
 		foreach(GameObject player in players)
 		{
-			if(Input.GetAxis("Door"+player.GetComponent<FPSInputController>().PlayerId) == 1 && delay != true && (player.transform.position - gameObject.transform.position).magnitude <= 10)
+			if(Input.GetAxis("Door"+player.GetComponent<FPSInputController>().PlayerId) == 1 && delay != true && (transform.position - player.transform.position).magnitude <= 25)
 			{
-				delay = true;
-				if(open)
-				{
-					Debug.Log ("Closed");
-					StartCoroutine("Close");
+				Debug.Log ("Pressed open door");
+				//hit = Physics.CapsuleCastAll(gameObject.transform.position, gameObject.GetComponent<BoxCollider>().center + Vector3.up * (15*0.5f), 10, transform.right, 10f);
+				//foreach(RaycastHit hitThing in hit)
+				//{
+					//Debug.Log (hitThing.collider.gameObject.name);
+					////if(hitThing.collider.gameObject == player)
+					//{
+						delay = true;
+						if(open)
+						{
+							Debug.Log ("Closed");
+							StartCoroutine("Close");
 
-				}
-				else
-				{
-					Debug.Log ("Open");
-					StartCoroutine("Open");
-				}
+						}
+						else
+						{
+							Debug.Log ("Open");
+							StartCoroutine("Open");
+						}
+					//}
+				//}
+
+			}
+			else if(Input.GetAxis("Door"+player.GetComponent<FPSInputController>().PlayerId) == 1 && delay != true && (transform.position - player.transform.position).magnitude > 20)
+			{
+				Debug.Log ((transform.position - player.transform.position).magnitude);
 			}
 		}
 		
